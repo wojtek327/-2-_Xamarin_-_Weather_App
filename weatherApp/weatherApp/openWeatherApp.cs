d0b9e4a7682192237792474cafec2c8848af1ecc
Wojciech Kloc <wojtek327@gmail.com>
2022-11-07 01:23:05 +0100

    Xamarin Weather App V1.0

---
+++ weatherApp/weatherApp/openWeatherApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace weatherApp
+{
+    class openWeatherApp
+    {
+        public class OpenWeatherMap<T>
+        {
+            private const string OpenWeatherApi = "http://api.openweathermap.org/data/2.5/weather?q=";
+            private const string Key = "653b1f0bf8a08686ac505ef6f05b94c2";
+            HttpClient _httpClient = new HttpClient();
+
+            public async Task<T> GetAllWeathers(string city)
+            {
+                var json = await _httpClient.GetStringAsync(OpenWeatherApi + city + "&APPID=" + Key);
+                var getWeatherModels = JsonConvert.DeserializeObject<T>(json);
+                return getWeatherModels;
+            }
+        }
+    }
+}
